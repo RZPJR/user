54ea8bb110be7d519a417628b26022e42f5aec24
Dindin Mahpudin <mahpudeen@gmail.com>
2023-02-09 16:53:03 +0700

    breadcrumb

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
-  <v-app>
-    <div id="microApp">
-      <v-main>
-        <router-view></router-view>
-      </v-main>
-    </div>
-  </v-app>
+    <v-app>
+        <div id="microApp">
+            <v-main>
+                <router-view></router-view>
+            </v-main>
+        </div>
+    </v-app>
 </template>
-
+<script>
+export default {
+    watch: { '$route' (val) { localStorage.setItem("route", JSON.stringify(val)); } },
+}
+</script>
