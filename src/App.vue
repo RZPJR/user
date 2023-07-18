8de2b4ff147f7ed23e3edafb121f23b60126242b
Tebe <zaki.baihaki@edenfarm.id>
2023-07-18 16:16:24 +0700

    Update Router

--- src/App.vue
+++ src/App.vue
@@ -9,13 +9,37 @@
 </template>
 <script>
 export default {
-    watch: { '$route' (val) { this.setRouter(val) } },
+    watch: { '$route' (val) { this.setRouter(val), this.checkBrowserTab(val) } },
     created() {
         this.setRouter(this.$router.history.current)
+        this.checkBrowserTab(this.$router.history.current)
     },
     methods : {
         setRouter(val) {
-            localStorage.setItem("route", JSON.stringify(val));
+            const shallowCopy = {...val};
+            localStorage.setItem("route", JSON.stringify(shallowCopy));
+        },
+        checkBrowserTab(val) {
+            document.addEventListener("visibilitychange", () => {
+                if (document.visibilityState === "visible" && val.name !== null) {
+                    const dataRoute = {
+                        fullPath: val.fullPath,
+                        hash: val.hash,
+                        matched: val.matched,
+                        meta: val.meta,
+                        name: val.name,
+                        params: val.params,
+                        path: val.path,
+                        query: val.query,
+                    }
+                    // Create a shallow copy of the object without circular properties
+                    const clonedDataRoute = {...dataRoute};
+                    delete clonedDataRoute.matched;
+                    const dataRouteString = JSON.stringify(clonedDataRoute); // Convert object to string
+                    localStorage.setItem("route", dataRouteString);
+                   
+                }
+            });
         }
     }
 }
