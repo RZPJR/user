5947637e163966d086787ed107f40008b26c6a39
Junior <junior@edenfarm.id>
2023-09-19 07:52:55 +0000

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 module.exports = {
+  productionSourceMap: true,
   lintOnSave: false,
   configureWebpack: {
+    mode: 'production',
     devServer: {
       headers: {
         "Access-Control-Allow-Origin": "*"
